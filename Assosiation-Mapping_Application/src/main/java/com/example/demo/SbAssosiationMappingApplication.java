f184ca366440c0a5809f787a3c1bb835598c6400
Chandu1234554321 <155133036+Chandu1234554321@users.noreply.github.com>
2025-02-01 00:06:06 -0600

    Add files via upload

---
+++ Assosiation-Mapping_Application/src/main/java/com/example/demo/SbAssosiationMappingApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SbAssosiationMappingApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext  context=SpringApplication.run(SbAssosiationMappingApplication.class, args);
+	    
+	
+	}
+
+}
